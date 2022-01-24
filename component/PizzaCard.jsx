b3a4ff393f61e6a5a5785040c12c269b4ffb37a0
Joyce Malicha <jmalisha22@gmail.com>
2022-01-24 12:34:44 +0300

    first commit

--- component/PizzaCard.jsx
+++ component/PizzaCard.jsx
@@ -6,7 +6,7 @@ const PizzaCard = () => {
   return (
     <div className={styles.container}>
       <Image src='/img/p7.jpg' alt='' width='500px' height='500px' />
-       <h1 className={styles.title}>HAWAIIAN</h1>
+       <h1 className={styles.title}>HAWAAIAN</h1>
        <span className={styles.price}>KSH 1200</span>
       <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quod porro excepturi unde magni</p>
     </div>
